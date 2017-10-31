7aab57d8f957c18deba85486fc8830aa6792eb46
Ale Ksandar <88aleksandar88@gmail.com>
2017-10-31 20:39:45 +0100

    Adding icons to todo list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,18 +8,23 @@ import { NavController, NavParams } from 'ionic-angular';
 export class HomePage {
 
 	selectedItem: any;
-	items: Array<{title: string, note: string}>;
-
+	items: Array<{title: string, note: string, icon: string}>;
+	icons: string[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
+
+  	this.icons = [
+  		'flask', 'wifi', 'beer', 'football', 'bluetooth', 'boat'
+  	];
   	this.selectedItem = navParams.get('item');
   	this.items = [];
   	for (let i = 1; i <= 10; i++){
   		this.items.push(
 	  		{
 	  			title: 'Item' + i,
-	  			note: 'This is item #' + i
+	  			note: 'This is item #' + i,
+	  			icon: this.icons[i - 1]
 	  		}
   		);
   	}
